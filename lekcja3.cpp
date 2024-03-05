67148348c1091703a141aae39d50afa9dd5b4ee7
Palix359 <80878735+Palix359@users.noreply.github.com>
2024-03-05 09:51:54 +0100

    Update lekcja3.cpp

--- lekcja3.cpp
+++ lekcja3.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-	srand(time(0)); //srand(time(NULL))
+	srand(time(0)); //srand(time(NULL)) - to jest silnik renderujący
 	int i = 10;
 	do {
 		cout << i-- << endl;
@@ -18,9 +18,36 @@ int main() {
 	
 	i = 1;
 	do {
-		cout << rand() << endl;
+		cout << rand()%10+1 << endl;
+		// <a;b>
+		// rand()%(b-a+1)+a
 		i++;
 	} while (i <= 100);
 
+	/* 
+	Zadula
+	1) komp 3
+	u: 5 liczba jest za duża
+	u: 1 liczba jest za mała
+	u: 3 wygrana 
+	*/
+	/*short k = rand() % 10 + 1;
+	int a = 0;
+	int j = 3;
+	do {
+		cout << "Podaj liczbe: ";
+		cin >> a;
+		if (a == k) {
+			cout << "Wygrales\n";
+		}
+		else if (a < k) {
+			cout << "Podaj wieksza liczbe\n";
+		}
+		else {
+			cout << "Podaj mniejsza liczbe\n";
+		}
+		j--;
+	} while (a != k && j<0);*/
+
 	return 0;
-}
+	}
