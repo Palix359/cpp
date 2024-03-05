5b07add36e9c66d99fccf47c142cfebe671b3e41
Palix359 <80878735+Palix359@users.noreply.github.com>
2024-03-05 10:11:05 +0100

    Update lekcja3.cpp

--- lekcja3.cpp
+++ lekcja3.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
 	srand(time(0)); //srand(time(NULL)) - to jest silnik renderujący
-	int i = 10;
+	/*int i = 10;
 	do {
 		cout << i-- << endl;
-	} while (i >= 1);
+	} while (i >= 1);*/
 	/*
 	do {
 		cout << i-- << endl;
@@ -16,13 +16,13 @@ int main() {
 	
 	//Zgadula <1;10> 3x
 	
-	i = 1;
-	do {
-		cout << rand()%10+1 << endl;
-		// <a;b>
-		// rand()%(b-a+1)+a
-		i++;
-	} while (i <= 100);
+	//i = 1;
+	//do {
+	//	cout << rand()%10+1 << endl;
+	//	// <a;b>
+	//	// rand()%(b-a+1)+a
+	//	i++;
+	//} while (i <= 100);
 
 	/* 
 	Zadula
@@ -48,6 +48,18 @@ int main() {
 		}
 		j--;
 	} while (a != k && j<0);*/
-
+	/*int a = 0;
+	int b = 0;
+	int c = 0;
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+	do {
+		c = a % b;
+		a = b;
+		b = c;
+	} while (b > 0);
+	cout << "NWD = " << a;*/
 	return 0;
 	}
