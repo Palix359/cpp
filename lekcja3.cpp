1ae91f608196d9cf4999d49007bd883040dbd090
Palix359 <80878735+Palix359@users.noreply.github.com>
2024-03-05 09:15:53 +0100

    Update lekcja3.cpp

--- lekcja3.cpp
+++ lekcja3.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
 
+int main() {
+	srand(time(0)); //srand(time(NULL))
+	int i = 10;
+	do {
+		cout << i-- << endl;
+	} while (i >= 1);
+	/*
+	do {
+		cout << i-- << endl;
+	} while (i <= 1);
+	*/
+	
+	//Zgadula <1;10> 3x
+	
+	i = 1;
+	do {
+		cout << rand() << endl;
+		i++;
+	} while (i <= 100);
+
+	return 0;
+}
