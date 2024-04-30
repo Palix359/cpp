b0e18dfbc47cbea49cb6e1bdc889a7b68c89dcf9
Palix359 <80878735+Palix359@users.noreply.github.com>
2024-04-30 10:08:15 +0200

    Update lekcja5.cpp

--- lekcja5.cpp
+++ lekcja5.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+enum Plec { kobieta, mezczyzna=123, inny };
+enum Odpowiedz { nie, tak, no=0, yes=1 };
+typedef unsigned long long Liczba;
+
+unsigned long long liczba1;
 int main() {
+	Liczba liczba = 123;
+	Plec uczen1 = inny;
+	cout << uczen1 << endl;;
 	int a = 123;
 	int* wsk=&a;
 	cout << a << ' ' << &a << endl;
@@ -15,12 +23,27 @@ int main() {
 	cout << "Ile liczb chcesz przechowac?\n";
 	int n; cin >> n;
 	int* tab = new int[n];
-	for (int i = 0; i < n; i++)
+	/*for (int i = 0; i < n; i++)
 		tab[i] = i;
 	for (int i = 0; i < n; i++)
-		cout << tab[i] << ' ';
+		cout << tab[i] << ' ';*/
 	delete[] tab;
 	
-	
+	//sito Eratostenesa
+	bool* sito = new bool[n + 1];
+	for (int i = 2; i < n; i++)
+		sito[i] = true;
+	int primeNumber = 2;
+	while (primeNumber <= sqrt(n)) {
+		while (sito[primeNumber] == false) primeNumber++;
+		for (int i = primeNumber * primeNumber; i < n; i += primeNumber)
+			if (sito[i] == true)
+				sito[i] = false;
+		primeNumber ++;
+	}
+	for (int i = 0; i < n + 1; i++)
+		if (sito[i] == true)
+			cout <<'\n' << i << ' ';
+	delete[] sito;
 	return 0;
 }
