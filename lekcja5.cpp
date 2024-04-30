e6ef3697ba99df658572f7ca083efc0a1668923d
Palix359 <80878735+Palix359@users.noreply.github.com>
2024-04-30 09:20:10 +0200

    Update lekcja5.cpp

--- lekcja5.cpp
+++ lekcja5.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main() {
-	
-	
+	int a = 123;
+	int* wsk=&a;
+	cout << a << ' ' << &a << endl;
+	cout << wsk << endl;
+	*wsk = 321;
+	int b = 997;
+	wsk = &b;
+	cout << a << endl;
+	cout << *wsk << endl;
+	//tabl. dynamiczne
+	cout << "Ile liczb chcesz przechowac?\n";
+	int n; cin >> n;
+	int* tab = new int[n];
+	for (int i = 0; i < n; i++)
+		tab[i] = i;
+	for (int i = 0; i < n; i++)
+		cout << tab[i] << ' ';
+	delete[] tab;
 	
 	
 	return 0;
